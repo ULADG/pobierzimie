bc4dfdcd090d6b3e0fb7ea2a5a1e0bb9ef2253a3
Urszula Dziewit <urszula.dziewit@inwenta.pl>
2017-06-17 09:43:08 +0200

    final

---
+++ js/zmienne.js
@@ -0,0 +1,8 @@
+"use strict"
+var formularz = document.getElementById('formularz');
+
+console.log(formularz);
+var fimie = document.getElementsByName('Krystian');
+console.log;
+var lnazwisko = document.getElementsByName('Dziopa');
+console.log;
